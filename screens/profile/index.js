e7459ec9cff20cf4835756c90e8b5176597d9841
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2022-03-15 21:09:42 +0800

    [Profile] added clickable favorite carparks and added a simple style

--- screens/profile/index.js
+++ screens/profile/index.js
@@ -1,21 +1,61 @@
 import React from 'react';
-import {Button, SafeAreaView, Text, View} from 'react-native';
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {delToken} from '../common/authorization';
 import styles from './styles';
 import {useDispatch, useSelector} from 'react-redux';
 import {setLogout} from '../../slices/isLoggedSlice';
+import search from '../search/styles';
+import typography from '../common/typography';
 
-export default function Profile() {
+export default function Profile(props) {
   const dispatch = useDispatch();
   const data = useSelector(state => state.isLogged);
 
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <Text>{data.name}</Text>
-        {data.favorite.map(favorite => {
-          return <Text key={favorite.favorite_id}>{favorite.carpark_id}</Text>;
-        })}
+      <View style={styles.nameContainer}>
+        <Text style={[typography.text, {fontSize: 17, fontWeight: '800'}]}>
+          USERNAME - {data.name}
+        </Text>
+      </View>
+      <View style={styles.emailContainer}>
+        <Text style={[typography.text, {fontSize: 17, fontWeight: '800'}]}>
+          EMAIL - {data.email}
+        </Text>
+      </View>
+      <View style={styles.favoriteContainer}>
+        <Text style={[typography.text, {fontSize: 17, fontWeight: '800'}]}>
+          Favorites
+        </Text>
+        <ScrollView>
+          {data.favorite.length > 0 ? (
+            data.favorite.map(favorite => {
+              return (
+                <TouchableOpacity
+                  onPress={() => {
+                    props.navigation.navigate(favorite.park_number);
+                  }}
+                  key={favorite._id}
+                  style={[search.carparkContainer, {justifyContent: 'center'}]}>
+                  <Text style={typography.text}>
+                    {favorite.park_address} ({favorite.park_number})
+                  </Text>
+                </TouchableOpacity>
+              );
+            })
+          ) : (
+            <Text>You have no favorites yet!</Text>
+          )}
+        </ScrollView>
+      </View>
+      <View style={styles.logoutContainer}>
         <Button
           title="Logout"
           onPress={() => {
--- screens/profile/styles.js
+++ screens/profile/styles.js
@@ -3,8 +3,25 @@ import {StyleSheet} from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'white',
+  },
+  nameContainer: {
+    flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    padding: 20,
+  },
+  emailContainer: {
+    flex: 1,
+    padding: 20,
+  },
+  favoriteContainer: {
+    flex: 10,
+    padding: 20,
+  },
+  logoutContainer: {
+    position: 'absolute',
+    alignSelf: 'center',
+    bottom: 0,
   },
 });
 
