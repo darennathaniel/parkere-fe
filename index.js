8e315565eb0b1f542c95b14c4ac6b8bd33c71a23
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2022-03-29 21:52:43 +0800

    [Expo] added register to expo

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
  */
 
 import {AppRegistry} from 'react-native';
+import {registerRootComponent} from 'expo';
 import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
-AppRegistry.registerComponent(appName.toLowerCase(), () => App);
+registerRootComponent(App);
