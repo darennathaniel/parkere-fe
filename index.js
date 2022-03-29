90e97c5fe1707e3c0bd74f82d3a2317575fdcafd
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2022-03-29 22:06:23 +0800

    [Bugs] fixed bugs

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
-registerRootComponent(App);
+// registerRootComponent(App);
